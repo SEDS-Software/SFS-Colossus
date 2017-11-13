16c086ea2d7ff571f6d74615019278220c323b25
Sumeet Bansal <sumeetbansal@gmail.com>
2017-11-12 17:40:32 -0800

    cleaned up and documented the PNID controller

--- src/main/java/GUI/ControllerPNID.java
+++ src/main/java/GUI/ControllerPNID.java
@@ -14,14 +14,21 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.Set;
 
+/**
+ * Controls the PNID screen.
+ */
 public class ControllerPNID {
 
-	@FXML private Fun_Rect pBar786;
-	@FXML private Fun_Rect pBar707;
+	// initializes the fuel and LOX bars
+	@FXML private Fun_Rect fuel786;
+	@FXML private Fun_Rect lox707;
+
 	private double prevLevel786;
 	private double prevLevel707;
 
 	@FXML private Rectangle botN21;
+
+	// initializes the valves, each of which has an open and closed state
 	@FXML private ImageView open252;
 	@FXML private ImageView open352;
 	@FXML private ImageView open350;
@@ -45,6 +52,8 @@ public class ControllerPNID {
 	@FXML private ImageView closed151;
 	@FXML private ImageView closedTop;
 
+	// initializes the pressure transducer readings
+	private Set<Label> psysLabels;
 	@FXML private Label PT120;
 	@FXML private Label PT121;
 	@FXML private Label PT1221;
@@ -60,6 +69,8 @@ public class ControllerPNID {
 	@FXML private Label PT420;
 	@FXML private Label PT421;
 
+	// initializes the temperature readings
+	private Set<Label> temperatureLabels;
 	@FXML private Label T290;
 	@FXML private Label T291;
 	@FXML private Label T292;
@@ -69,57 +80,84 @@ public class ControllerPNID {
 	@FXML private Label T392;
 	@FXML private Label T393;
 
-	Set<Label> psysLabs;
-	Set<Label> tempLabs;
-
+	// files that contain the valve readings
 	private String[] valveFiles = {"Val150", "Val151", "Val250", "Val251", "Val252", "Val253", "Val350", "Val351",
 			"Val352", "Val353"};
-	private String[] tempFiles = {"T290", "T291", "T292", "T293", "T390", "T391", "T392", "T393"};
-	private boolean[] currState = {true, true, true, true, true, true, true, true, true, true};
+	private String[] temperatureFiles = {"T290", "T291", "T292", "T293", "T390", "T391", "T392", "T393"};
+	private boolean[] valveStates;
 
+	// starting values for pressure and temperature
+	private final double DEFAULT_PRESSURE = 0;
+	private final double DEFAULT_TEMPERATURE = 0;
 
-	//These are going to be used for a possible debug mode
-	/*
+	// lox bar positioning
+	private final int LOX_WIDTH = 65;
+	private final int LOX_HEIGHT = 123;
+	private final int LOX_X = 1384;
+	private final int LOX_Y = 557;
+
+	// fuel tank positioning
+	private final int FUEL_WIDTH = LOX_WIDTH;
+	private final int FUEL_HEIGHT = LOX_HEIGHT;
+	private final int FUEL_X = 315;
+	private final int FUEL_Y = LOX_Y;
+
+	// thread sleep time (in milliseconds)
+	private final int SLEEP_TIME = 100;
+
+	/* to be used for a possible debug mode
 	private boolean botN21Hovered = false;
 	private boolean botN22Hovered= false;
 	private boolean botN23Hovered = false;
 	private boolean botN24Hovered = false;
 	*/
 
+	/**
+	 * Initializes the necessary variables and runs the PNID in its own thread indefinitely.
+	 */
 	@FXML
 	private void initialize() {
 
-		psysLabs = new HashSet<>();
+		// initializes the label sets--must be done here because of how the FXML initializes the labels
+		psysLabels = new HashSet<>();
 		Label[] PTLabels = {PT120, PT121, PT1221, PT1222, PT123, PT220, PT221, PT222, PT223, PT321, PT322, PT323,
 				PT420, PT421};
-		psysLabs.addAll(Arrays.asList(PTLabels));
-		tempLabs = new HashSet<>();
+		psysLabels.addAll(Arrays.asList(PTLabels));
+		temperatureLabels = new HashSet<>();
 		Label[] TLabels = {T290, T291, T292, T293, T390, T391, T392, T393};
-		tempLabs.addAll(Arrays.asList(TLabels));
+		temperatureLabels.addAll(Arrays.asList(TLabels));
 
-		for (Label label : psysLabs) {
-			label.setText("0");
+		// sets the pressure and temperature labels to the appopriate starting values
+		for (Label label : psysLabels) {
+			label.setText(Long.toString(Math.round(DEFAULT_PRESSURE)));
 		}
-		for (Label label : tempLabs) {
-			label.setText("0");
+		for (Label label : temperatureLabels) {
+			label.setText(Long.toString(Math.round(DEFAULT_TEMPERATURE)));
 		}
 
-		pBar707.setPrefWidth(65);
-		pBar707.setPrefHeight(123);
-		pBar707.setLayoutX(1384);
-		pBar707.setLayoutY(557);
+		// sets the state of all valves to true
+		valveStates = new boolean[valveFiles.length];
+		for (int i = 0; i < valveStates.length; i++) {
+			valveStates[i] = true;
+		}
 
-		pBar786.setPrefWidth(65);
-		pBar786.setPrefHeight(123);
-		pBar786.setLayoutX(315);
-		pBar786.setLayoutY(557);
+		lox707.setPrefWidth(LOX_WIDTH);
+		lox707.setPrefHeight(LOX_HEIGHT);
+		lox707.setLayoutX(LOX_X);
+		lox707.setLayoutY(LOX_Y);
 
+		fuel786.setPrefWidth(FUEL_WIDTH);
+		fuel786.setPrefHeight(FUEL_HEIGHT);
+		fuel786.setLayoutX(FUEL_X);
+		fuel786.setLayoutY(FUEL_Y);
+
+		// runs the PNID indefinitely
 		new Thread(() -> {
 			while (true) {
-				updateVal();
+				updateValues();
 				updateState();
 				try {
-					Thread.sleep(100);
+					Thread.sleep(SLEEP_TIME);
 				} catch (InterruptedException e) {
 					System.out.println("Process interrupted.");
 				}
@@ -128,51 +166,63 @@ public class ControllerPNID {
 
 	}
 
-	private void updateVal() {
+	/**
+	 * Updates the values for the pressure and temperature readings as discrete values.
+	 */
+	private void updateValues() {
+
+		// iterates through the PTx files and updates the pressure labels
 		Platform.runLater(new Runnable() {
 			public void run() {
 				int i = 1;
-
-				for (Label psysLabel : psysLabs) {
+				for (Label label : psysLabels) {
 					File file = new File("PT" + i++);
 					try {
 						Scanner scanner = new Scanner(file);
-						psysLabel.setText(Long.toString(Math.round(scanner.nextDouble())));
+						label.setText(Long.toString(Math.round(scanner.nextDouble())));
 						scanner.close();
 					} catch (FileNotFoundException e) {
-						System.out.println("File not found: " + file.getAbsolutePath());
+						System.out.println("File not found: " + file.getName());
 					} catch (NoSuchElementException e) {
+
+						// can be caused by the file being written to while attempting to read
 						System.out.println("Unable to read file: PT" + (i-1));
+
 					}
 				}
 			}
 		});
+
+		// iterates through the Tx files and updates the temperature labels
 		Platform.runLater(new Runnable() {
 			public void run() {
 				int i = 0;
-				for (Label tempLabel : tempLabs) {
-					File file = new File("" + tempFiles[i++]);
+				for (Label label : temperatureLabels) {
+					File file = new File(temperatureFiles[i++]);
 					try {
 						Scanner scanner = new Scanner(file);
-						tempLabel.setText(Long.toString(Math.round(scanner.nextDouble())));
+						label.setText(Long.toString(Math.round(scanner.nextDouble())));
 						scanner.close();
 					} catch (FileNotFoundException e) {
-						System.out.println("File not found: " + file.getAbsolutePath());
+						System.out.println("File not found: " + file.getName());
 					} catch (NoSuchElementException e) {
-						System.out.println("Unable to read file: " + tempFiles[i-1]);
-					}
 
+						// can be caused by the file being written to while attempting to read
+						System.out.println("Unable to read file: PT" + (i-1));
+
+					}
 				}
 			}
 		});
 
+		// iterates through the fuel tank files and updates the fuel labels
 		File file = null;
 		try {
 			file = new File("Tank_Fuel_1");
 			Scanner scanner = new Scanner(file);
 			double funLevel707 = scanner.nextDouble() / 100;
 			if (funLevel707 != prevLevel707) {
-				pBar707.setLevel(funLevel707);
+				lox707.setLevel(funLevel707);
 				prevLevel707 = funLevel707;
 			}
 			scanner.close();
@@ -181,39 +231,52 @@ public class ControllerPNID {
 			scanner = new Scanner(file);
 			double funLevel786 = scanner.nextDouble() / 100;
 			if (funLevel786 != prevLevel786) {
-				pBar786.setLevel(funLevel786);
+				fuel786.setLevel(funLevel786);
 				prevLevel786 = funLevel786;
 			}
 			scanner.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found: " + file.getName());
+		} catch (NoSuchElementException e) {
+
+			// can be caused by the file being written to while attempting to read
+			System.out.println("Unable to read file: " + file.getName());
+
 		}
 	}
 
+	/**
+	 * Updates the internal values for the valve states. Used by updateState().
+	 */
 	private void getState() {
 		for (int i = 0; i < valveFiles.length; i++) {
 			try {
 				File file = new File("" + valveFiles[i]);
 				Scanner scanner = new Scanner(file);
-
-				currState[i] = (scanner.nextDouble() > 0);
+				valveStates[i] = (scanner.nextDouble() > 0);
 				scanner.close();
-
-			} catch (Exception e) {
-				//do nothing
+			} catch (FileNotFoundException e) {
+				System.out.println("File not found: " + valveFiles[i]);
+			} catch (NoSuchElementException e) {
+				System.out.println("Unable to read file: " + valveFiles[i]);
 			}
 		}
 	}
 
+	/**
+	 * Switches valve states between open/closed if a change is detected.
+	 */
 	private void updateState() {
 		getState();
+
+		// pre-update states can be determined by the visibility of the open/closed images
 		ImageView[] prevStates = {open150, open151, open250, open251, open252, open253, open350, open351, open352,
 				open353};
 		Platform.runLater(new Runnable() {
 			@Override
 			public void run() {
-				for (int i = 0; i < currState.length; i++) {
-					if (prevStates[i].isVisible() != currState[i]) {
+				for (int i = 0; i < valveStates.length; i++) {
+					if (prevStates[i].isVisible() != valveStates[i]) {
 						switch (i) {
 							case 0:
 								switchValve(closed150, open150);
@@ -253,9 +316,15 @@ public class ControllerPNID {
 
 	}
 
+	/**
+	 * Helper method to switch between open/closed valve states.
+	 * @param closed the "closed" image representation of the valve
+	 * @param open the "open" image representation of the valve
+	 */
 	private void switchValve(ImageView closed, ImageView open) {
 		boolean bool = closed.isVisible();
 		closed.setVisible(!bool);
 		open.setVisible(bool);
 	}
+
 }

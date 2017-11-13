668ac2d2f3660270123154b010a26bfbf3e07791
cyficowley <cyficowley@gmail.com>
2017-11-12 19:23:48 -0800

    Made the GUIs scalable They do not auto scale because I could not get the math to ever work out.  So I just made it so it is really easy to change the scaling manually. Basically you just change the constants in Main.java to whatever you need them to be.

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -5,10 +5,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	private static final double PNID_SCALE = .711;
+	private static final double VG_SCALE = .61;
+	private static final double EC_SCALE = .64;
+
+
+	private static final double PNID_OFFSET = 2;
+	private static final double VG_OFFSET = 1.8;
+	private static final double EC_OFFSET = 1.8;
+
+
+	private static final double screenWidth = 1280;
+	private static final double screenHeight = 720;
 
 	public Main() {
 	}
@@ -16,12 +29,18 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 
+
 		FXMLLoader loader = new FXMLLoader(Main.class.getResource("/VG_Fitted.fxml"));
 		Pane pane = (Pane)loader.load();
 		ControllerVG contr1 = loader.getController();
 		//ControllerVG contr1 = new ControllerVG();
 		//contr1.setMain(this); //give controller access to main methods such as updateChart
 		Scene scene = new Scene(pane);
+		scene.setFill(Color.BLACK);
+		pane.setScaleX(VG_SCALE);
+		pane.setScaleY(VG_SCALE);
+		pane.setTranslateX(-screenWidth * (1-VG_SCALE)/VG_OFFSET);
+		pane.setTranslateY(-screenHeight * (1-VG_SCALE)/VG_OFFSET);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
@@ -34,30 +53,28 @@ public class Main extends Application {
 		loader.setController(contPNID);
 
 		scene = new Scene(pane2);
+		pane2.setScaleX(PNID_SCALE);
+		pane2.setScaleY(PNID_SCALE);
+		pane2.setTranslateX(-screenWidth * (1-PNID_SCALE)/PNID_OFFSET);
+		pane2.setTranslateY(-screenHeight * (1-PNID_SCALE)/PNID_OFFSET);
 		stage.setScene(scene);
 		stage.show();
 
-		//DoubleController dubCont = new DoubleController( contr1, contPNID );
-		/*new Thread(() -> {
-			while (true) {
-				contr1.updateVal();
-				try {
-					Thread.sleep(100);
-				} catch (Exception e) {
-					System.out.println("sleep error");
-				}
-			}
-		}).start();*/
 
 
 		Stage newstage = new Stage();
 		newstage.setTitle("Executive Cue");
 		loader = new FXMLLoader(Main.class.getResource("/ExecCue_Fitted.fxml"));
 		Pane newpane2 = (Pane)loader.load();
+
+		newpane2.setScaleX(EC_SCALE);
+		newpane2.setScaleY(EC_SCALE);
+		newpane2.setTranslateX(-screenWidth * (1-EC_SCALE)/EC_OFFSET);
+		newpane2.setTranslateY(-screenHeight * (1-EC_SCALE)/EC_OFFSET);
 		scene = new Scene(newpane2);
+		scene.setFill(Color.BLACK);
 		newstage.setScene(scene);
 		newstage.show();
-
 	}
 
 

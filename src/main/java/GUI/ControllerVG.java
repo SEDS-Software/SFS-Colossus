7a2ff97a4429196e65c6d988f34bdf238a00cea8
Sumeet Bansal <sumeetbansal@gmail.com>
2018-01-13 16:35:18 -0800

    refactored the VG controller

--- src/main/java/GUI/ControllerVG.java
+++ src/main/java/GUI/ControllerVG.java
@@ -8,7 +8,7 @@ import javafx.scene.chart.*;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Stop;
-import java.util.LinkedList;
+
 import java.util.*;
 import java.io.*;
 
@@ -29,25 +29,18 @@ public class ControllerVG {
 	@FXML private Gauge psys12;
 	@FXML private Gauge psys13;
 	@FXML private Gauge psys14;
-	//@FXML private Gauge Tank_Fuel_1;
 	@FXML private FunLevel Tank_Fuel_1;
-	//@FXML private Gauge Tank_PT_1;
 	@FXML private Gauge Tank_Temp_1;
-	//@FXML private Gauge Tank_Fuel_2;
 	@FXML private FunLevel Tank_Fuel_2;
-	//@FXML private Gauge Tank_PT_2;
 	@FXML private Gauge Tank_Temp_2;
 	@FXML private Gauge battery;
 	@FXML private Gauge Thrust_Gauge;
 	private double thrustVal;
-	private double prevFunLevel1;
-	private double prevFunLevel2;
-	private double currFunLevel1;
-	private double currFunLevel2;
 
-	private Gauge[] psys = {psys1, psys2,psys3,psys4, psys5, psys6, psys7,psys8,psys9, psys10, psys11, psys12,psys13,psys14};
-	private double [] psysVals = new double [14];
+	private Set<Gauge> psysGauges;
 
+	// thread sleep time (in milliseconds)
+	private final int SLEEP_TIME = 100;
 
 	@FXML private LineChart <String, Number> chart1;
 	@FXML private LineChart <String, Number> chart2;
@@ -55,39 +48,43 @@ public class ControllerVG {
 	private XYChart.Series<String, Number> series1 = new XYChart.Series<>();
 	private XYChart.Series<String, Number> series2 = new XYChart.Series<>();
 	private XYChart.Series<String, Number> seriesThrust = new XYChart.Series<>();
-	private int xval = 12;
-	private int timeToUpdate = 0;
-
-
+	private int chartTime = 12;
+	private int chartCount = 0;
 
 	@FXML
 	private void initialize() {
-		Gauge[] psys = {psys1, psys2,psys3,psys4, psys5, psys6, psys7,psys8,psys9, psys10, psys11, psys12,psys13,psys14};
 
-		//Tank_Fuel_1.setGradientBarEnabled(true);
-		//Tank_Fuel_2.setGradientBarEnabled(true);
-		LinkedList<Stop> stops = new LinkedList<Stop>();
-		stops.add(new Stop(0.0, Color.RED));
-		stops.add(new Stop(.50, Color.YELLOW));
+		// initializes the gauge set--must be done here because of how the FXML initializes the gauges
+		psysGauges = new LinkedHashSet<>();
+		Gauge[] psys = {psys1, psys2, psys3, psys4, psys5, psys6, psys7, psys8, psys9, psys10, psys11, psys12,
+				psys13, psys14};
+		psysGauges.addAll(Arrays.asList(psys));
+
+		List<Stop> stops = new LinkedList<>();
+		stops.add(new Stop(0.00, Color.RED));
+		stops.add(new Stop(0.50, Color.YELLOW));
 		stops.add(new Stop(1.00, Color.GREEN));
-		//Tank_Fuel_1.setGradientBarStops(stops);
-		//Tank_Fuel_2.setGradientBarStops(stops);
 
-		//battery in the top right corner
+//		Tank_Fuel_1.setGradientBarEnabled(true);
+//		Tank_Fuel_1.setGradientBarStops(stops);
+//		Tank_Fuel_2.setGradientBarEnabled(true);
+//		Tank_Fuel_2.setGradientBarStops(stops);
+
+		// battery in the top right corner
 		battery.setGradientBarEnabled(true);
 		battery.setGradientBarStops(stops);
 
-		LinkedList<Section> secs = new LinkedList<>();
+		List<Section> secs = new LinkedList<>();
 		secs.add(new Section(0.0, 100.0, Color.RED));
 		secs.add(new Section(100.0, 1300.0, Color.GREEN));
 		secs.add(new Section(1300.0, 1440.0, Color.ORANGE));
 		secs.add(new Section(1440.0, 1600.0, Color.RED));
-		for (int i=0; i<psys.length; i++) {
-			psys[i].setSections(secs);
+		for (Gauge psysGauge : psysGauges) {
+			psysGauge.setSections(secs);
 		}
 
-		//Tank_PT_1.setSections(secs);
-		//Tank_PT_2.setSections(secs);
+//		Tank_PT_1.setSections(secs);
+//		Tank_PT_2.setSections(secs);
 
 		LinkedList<Section> temp = new LinkedList<>();
 		temp.add(new Section(0.0, 700.0, Color.GREEN));
@@ -96,16 +93,13 @@ public class ControllerVG {
 		Tank_Temp_1.setSections(temp);
 		Tank_Temp_2.setSections(temp);
 
-		for (int i=0; i<psys.length; i++) {
-			psys[i].setValueColor(Color.WHITE);
+		for (Gauge psysGauge : psysGauges) {
+			psysGauge.setValueColor(Color.WHITE);
+			psysGauge.setUnitColor(Color.WHITE);
 		}
 
-		//Tank_PT_1.setValueColor(Color.WHITE);
-		//Tank_PT_2.setValueColor(Color.WHITE);
-
-		for (int i=0; i<psys.length; i++) {
-			psys[i].setUnitColor(Color.WHITE);
-		}
+//		Tank_PT_1.setValueColor(Color.WHITE);
+//		Tank_PT_2.setValueColor(Color.WHITE);
 
 		Tank_Temp_1.setUnitColor(Color.WHITE);
 		Tank_Temp_2.setUnitColor(Color.WHITE);
@@ -114,137 +108,81 @@ public class ControllerVG {
 		chart1.getYAxis().setTickLength(10.0);
 		chart1.getData().clear();
 		series1 = new XYChart.Series<>();
-		series1.getData().add(new XYChart.Data<>("1", 0));
-		series1.getData().add(new XYChart.Data<>("2", 0));
-		series1.getData().add(new XYChart.Data<>("3", 0));
-		series1.getData().add(new XYChart.Data<>("4", 0));
-		series1.getData().add(new XYChart.Data<>("5", 0));
-		series1.getData().add(new XYChart.Data<>("6", 0));
-		series1.getData().add(new XYChart.Data<>("7", 0));
-		series1.getData().add(new XYChart.Data<>("8", 0));
-		series1.getData().add(new XYChart.Data<>("9", 0));
-		series1.getData().add(new XYChart.Data<>("10", 0));
-		series1.getData().add(new XYChart.Data<>("11", 0));
-		series1.getData().add(new XYChart.Data<>("12", 0));
+		for (int i = 1; i <= psysGauges.size(); i++) {
+			series1.getData().add(new XYChart.Data<>(Integer.toString(i), 0));
+		}
 		chart1.getData().add(series1);
 
 		chart2.getYAxis().setAutoRanging(false);
 		chart2.getYAxis().setTickLength(10.0);
 		chart2.getData().clear();
 		series2 = new XYChart.Series<>();
-		series2.getData().add(new XYChart.Data<>("1", 0));
-		series2.getData().add(new XYChart.Data<>("2", 0));
-		series2.getData().add(new XYChart.Data<>("3", 0));
-		series2.getData().add(new XYChart.Data<>("4", 0));
-		series2.getData().add(new XYChart.Data<>("5", 0));
-		series2.getData().add(new XYChart.Data<>("6", 0));
-		series2.getData().add(new XYChart.Data<>("7", 0));
-		series2.getData().add(new XYChart.Data<>("8", 0));
-		series2.getData().add(new XYChart.Data<>("9", 0));
-		series2.getData().add(new XYChart.Data<>("10", 0));
-		series2.getData().add(new XYChart.Data<>("11", 0));
-		series2.getData().add(new XYChart.Data<>("12", 0));
+		for (int i = 1; i <= psysGauges.size(); i++) {
+			series2.getData().add(new XYChart.Data<>(Integer.toString(i), 0));
+		}
 		chart2.getData().add(series2);
 
 		chartThrust.getYAxis().setAutoRanging(false);
 		chartThrust.getYAxis().setTickLength(10.0);
 		chartThrust.getData().clear();
 		seriesThrust = new XYChart.Series<>();
-		seriesThrust.getData().add(new XYChart.Data<>("1", 0));
-		seriesThrust.getData().add(new XYChart.Data<>("2", 0));
-		seriesThrust.getData().add(new XYChart.Data<>("3", 0));
-		seriesThrust.getData().add(new XYChart.Data<>("4", 0));
-		seriesThrust.getData().add(new XYChart.Data<>("5", 0));
-		seriesThrust.getData().add(new XYChart.Data<>("6", 0));
-		seriesThrust.getData().add(new XYChart.Data<>("7", 0));
-		seriesThrust.getData().add(new XYChart.Data<>("8", 0));
-		seriesThrust.getData().add(new XYChart.Data<>("9", 0));
-		seriesThrust.getData().add(new XYChart.Data<>("10", 0));
-		seriesThrust.getData().add(new XYChart.Data<>("11", 0));
-		seriesThrust.getData().add(new XYChart.Data<>("12", 0));
+		for (int i = 1; i <= psysGauges.size(); i++) {
+			seriesThrust.getData().add(new XYChart.Data<>(Integer.toString(i), 0));
+		}
 		chartThrust.getData().add(seriesThrust);
 
 		new Thread(() -> {
 			while (true) {
 				updateVal();
 				try {
-					Thread.sleep(100);
+					Thread.sleep(SLEEP_TIME);
 				} catch (Exception e) {
-					System.out.println("sleep error");
+					System.out.println("Process interrupted.");
 				}
 			}
 		}).start();
 	}
 
-	private void getVal() {
-		Gauge[] psys = {psys1, psys2,psys3,psys4, psys5, psys6, psys7,psys8,psys9, psys10, psys11, psys12,psys13,psys14};
-		for(int i = 1; i<=psys.length; i++) {
-			try{
-				File file = new File("PT" + i);
-				Scanner scanner = new Scanner(file);
-
-				double retVal = scanner.nextDouble();
-				scanner.close();
-
-				psysVals[i-1] = retVal;
-				//psys[i].setValue(retVal);
-
-			}catch(Exception e) {
-				//do nothing
-			}
-		}
+	@FXML
+	private void updateVal() {
 		Platform.runLater(new Runnable() {
 			@Override
 			public void run() {
-				try{
-					File file = new File("Tank_Fuel_1");
-					Scanner scanner = new Scanner(file);
-					currFunLevel2 = scanner.nextDouble()/100;
-					if(currFunLevel2 != prevFunLevel2) {
-						Tank_Fuel_2.setLevel(currFunLevel2);
-						prevFunLevel2 = currFunLevel2;
+				int i = 1;
+				for (Gauge psysGauge : psysGauges) {
+					File file = new File("PT" + i++);
+					try {
+						Scanner scanner = new Scanner(file);
+						psysGauge.setValue(Math.round(scanner.nextDouble()));
+						scanner.close();
+					} catch (FileNotFoundException e) {
+						System.out.println("File not found: " + file.getName());
+					} catch (NoSuchElementException e) {
+
+						// can be caused by the file being written to while attempting to read
+						System.out.println("Unable to read file: " + file.getName());
+
 					}
+				}
+
+				File file = null;
+				Scanner scanner = null;
+				try {
+
+					file = new File("Tank_Fuel_1");
+					scanner = new Scanner(file);
+					Tank_Fuel_2.setLevel(scanner.nextDouble() / 100);
 					scanner.close();
 
 					file = new File("Tank_Fuel_2");
 					scanner = new Scanner(file);
-					currFunLevel1 = scanner.nextDouble()/100;
-					if(currFunLevel1 != prevFunLevel1) {
-						Tank_Fuel_1.setLevel(currFunLevel1);
-						prevFunLevel1 = currFunLevel1;
-					}
-					scanner.close();
-				} catch (Exception e){
-					//Do Nothing
-				}
-
-				try {
-					/*File file = new File("Tank_Fuel_1");
-					Scanner scanner = new Scanner(file);
-					double currFunLevel1 = scanner.nextDouble()/100;
-					if(currFunLevel1 != prevFunLevel1) {
-						Tank_Fuel_1.setLevel(currFunLevel1);
-						prevFunLevel1 = currFunLevel1;
-					}
+					Tank_Fuel_1.setLevel(scanner.nextDouble() / 100);
 					scanner.close();
 
-					Tank_PT_1.setValue((int)psysVals[12]);*/
-
-					File file = new File("Tank_Temp_1");
-					Scanner scanner = new Scanner(file);
+					file = new File("Tank_Temp_1");
+					scanner = new Scanner(file);
 					Tank_Temp_1.setValue(scanner.nextDouble());
 					scanner.close();
-
-					/*file = new File("Tank_Fuel_2");
-					scanner = new Scanner(file);
-					double currFunLevel2 = scanner.nextDouble()/100;
-					if(currFunLevel2 != prevFunLevel2) {
-						Tank_Fuel_2.setLevel(currFunLevel2);
-						prevFunLevel2 = currFunLevel2;
-					}
-					scanner.close();
-
-					Tank_PT_2.setValue((int)psysVals[13]);*/
 
 					file = new File("Tank_Temp_2");
 					scanner = new Scanner(file);
@@ -261,49 +199,34 @@ public class ControllerVG {
 					thrustVal = scanner.nextDouble();
 					Thrust_Gauge.setValue(thrustVal);
 					scanner.close();
-				}catch( Exception e) {
-					//do nothing;
+				} catch (FileNotFoundException e) {
+					System.out.println("File not found: " + file.getName());
+				} catch (NoSuchElementException e) {
+
+					// can be caused by the file being written to while attempting to read
+					System.out.println("Unable to read file: " + file.getName());
+
 				}
-			}
-		});
 
-	}
+				chartCount++;
+				if (chartCount % 10 == 0) {
+					chartTime++;
+					int nthLast = 0;
+					Gauge[] arr = psysGauges.toArray(new Gauge[0]);
 
-
-
-	@FXML
-	private void updateVal(){
-		getVal();
-		Gauge[] psys = {psys1, psys2,psys3,psys4, psys5, psys6, psys7,psys8,psys9, psys10, psys11, psys12,psys13,psys14};
-		Platform.runLater(new Runnable() {
-			@Override
-			public void run() {
-				for (int i=0; i<psys.length; i++) {
-					psys[i].setValue((int)psysVals[i]);
-				}
-			}
-		});
-
-		//allows the the chart to update in real time
-
-		timeToUpdate++;
-		if (timeToUpdate%10 == 0) {
-			Platform.runLater(new Runnable() {
-				public void run() {
-					int y = (int)psysVals[12];
+					double tank1Y = arr[arr.length - ++nthLast].getValue();
 					series1.getData().remove(0);
-					series1.getData().add(new XYChart.Data<>("" + ++xval, y));
+					series1.getData().add(new XYChart.Data<>(Integer.toString(chartTime), tank1Y));
 
-					y = (int)psysVals[13];
+					double tank2Y = arr[arr.length - ++nthLast].getValue();
 					series2.getData().remove(0);
-					series2.getData().add(new XYChart.Data<>("" + xval, y));
+					series2.getData().add(new XYChart.Data<>(Integer.toString(chartTime), tank2Y));
 
-					y = (int)thrustVal;
 					seriesThrust.getData().remove(0);
-					seriesThrust.getData().add(new XYChart.Data<>("" + xval, y));
+					seriesThrust.getData().add(new XYChart.Data<>(Integer.toString(chartTime), Math.round(thrustVal)));
 				}
-			});
-		}
+			}
+		});
 
 	}
 
